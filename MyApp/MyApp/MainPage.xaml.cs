e4bf60087bf65e497f9798269891ffe56f53d529
r4y856 <108265382+r4y856@users.noreply.github.com>
2022-07-01 15:22:19 +0100

    added xamarin project.

---
+++ MyApp/MyApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyApp
+{
+   
+    
+    public partial class MainPage : ContentPage
+    {
+    
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        async private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var newPage = new Page1();
+            await Navigation.PushAsync(newPage);
+        }
+        
+     
+
+
+
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+        void Entry_Completed(object sender, System.EventArgs e)
+        {
+            var label = new Label { Text = (sender).ToString() };
+            var text = ((Entry)sender).Text;
+            TeacherNameLabel.Text = ("Welcome, " + text);    
+        }
+    }
+}
---
+++ MyApp/MyApp/Page1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        void Entry_Completed1(object sender, System.EventArgs e)
+        {
+            var label = new Label { Text = (sender).ToString() };
+            var text = ((Entry)sender).Text;
+            Name.Text += (text);
+        }
+        void Entry_Completed2(object sender, System.EventArgs e)
+        {
+            var label = new Label { Text = (sender).ToString() };
+            var text = ((Entry)sender).Text;
+            Grade.Text += (text);
+        }
+    }
+}
